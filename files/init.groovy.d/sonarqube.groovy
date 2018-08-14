a0d15852374e1ecf15e617a2a1f82bae7ddf7cea
Grant Soyka <gsoyka@gmail.com>
2018-08-14 11:49:24 -0600

    Update sonarqube install script

--- files/init.groovy.d/sonarqube.groovy
+++ files/init.groovy.d/sonarqube.groovy
@@ -8,7 +8,7 @@ def desc = inst.getDescriptor("hudson.plugins.sonar.SonarRunnerInstallation")
 
 def installer = new SonarRunnerInstaller("[3.2.0.1227]")
 def prop = new InstallSourceProperty([installer])
-def sinst = new SonarRunnerInstallation("[sonar_3.2]", "[]", [prop])
+def sinst = new SonarRunnerInstallation("sonar_3.2", [prop])
 desc.setInstallations(sinst)
 
 desc.save()
